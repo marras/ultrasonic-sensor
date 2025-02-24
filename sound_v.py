dfda118456ffaacb0e6f9b0af5faa72ec552873e
Marek Waligórski <marek.waligorski@gmail.com>
2025-02-24 13:00:17 +0100

    Add script to simply check bounce time

---
+++ sound_v.py
@@ -0,0 +1,52 @@
+#Libraries
+import RPi.GPIO as GPIO
+import time
+ 
+#GPIO Mode (BOARD / BCM)
+GPIO.setmode(GPIO.BCM)
+ 
+#set GPIO Pins
+GPIO_TRIGGER = 17
+GPIO_ECHO = 4
+
+START_DIST = 65
+END_DIST = 3
+ 
+#set GPIO direction (IN / OUT)
+GPIO.setup(GPIO_TRIGGER, GPIO.OUT)
+GPIO.setup(GPIO_ECHO, GPIO.IN)
+ 
+def bounce_time():
+    # set Trigger to HIGH
+    GPIO.output(GPIO_TRIGGER, True)
+ 
+    # set Trigger after 0.01ms to LOW
+    time.sleep(0.00001)
+    GPIO.output(GPIO_TRIGGER, False)
+ 
+    # save StartTime
+    while GPIO.input(GPIO_ECHO) == 0:
+        StartTime = time.time()
+ 
+    # save time of arrival
+    while GPIO.input(GPIO_ECHO) == 1:
+        StopTime = time.time()
+ 
+    # time difference between start and arrival
+    TimeElapsed = StopTime - StartTime
+    # multiply with the sonic speed (34300 cm/s)
+    # and divide by 2, because there and back
+ 
+    return TimeElapsed
+ 
+if __name__ == '__main__':
+    try:
+        while True:
+            print ("Bounce time: %.2f", bounce_time())
+            
+            time.sleep(1.0) 
+ 
+        # Reset by pressing CTRL + C
+    except KeyboardInterrupt:
+        print("Thanks, bye!")
+        GPIO.cleanup()
